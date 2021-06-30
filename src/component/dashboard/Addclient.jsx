62728952a88b1861007d1b49a8d3ca61f0302953
Ashrof <salmanashraf853@gmail.com>
2021-06-30 13:47:17 +0100

    building add slient nav

--- src/component/dashboard/Addclient.jsx
+++ src/component/dashboard/Addclient.jsx
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Addclient extends Component {
-    render() {
-        return (
-            <div>
-                Add Client
-            </div>
-        )
+
+const Addclient =()=> {
+
+    const [phones, setPhones] = useState([])
+    const [phone, setPhone] = useState('')
+
+    const addPhone = (phone) => {
+        const phoneArray = phones
+
+        phoneArray.push(phone)
+        setPhones(phoneArray)
+        setPhone("")
     }
+
+
+    return (
+        <div className="addclient">
+            <p>Add new client</p>
+            <label>
+                Client Name
+                <input placeholder="Enter name" />
+            </label>
+            <label>
+                Client username
+                <input placeholder="Enter username" />
+            </label>
+
+            <div className="phonegrid">
+                {
+                    phones.map(phone=> {
+                        return <p>{phone}</p>
+                    })
+                }
+            </div>
+            <div className="phoneadder">
+                <input onChange={(e)=> setPhone(e.target.value)} value={phone} type="number" placeholder="Enter new mobile number" />
+                <button onClick={()=> addPhone(phone)}>+ADD</button>
+            </div>
+        </div>
+    )
 }
 
 export default Addclient
